7f7bc9497d1384d3cd7f366e49f7178e5a1b02ea
Roman <roman.panichkin@termius.com>
2021-06-14 16:33:00 +0600

    Change package bundle to be able to load .html resource

--- Sources/ReCaptcha/ReCaptcha.swift
+++ Sources/ReCaptcha/ReCaptcha.swift
@@ -55,14 +55,7 @@ public class ReCaptcha {
 
         /// The Bundle that holds ReCaptcha's assets
         private static let bundle: Bundle = {
-            let bundle = Bundle(for: ReCaptcha.self)
-            guard let cocoapodsBundle = bundle
-                .path(forResource: "ReCaptcha", ofType: "bundle")
-                .flatMap(Bundle.init(path:)) else {
-                    return bundle
-            }
-
-            return cocoapodsBundle
+            return Bundle.module
         }()
 
         /**
